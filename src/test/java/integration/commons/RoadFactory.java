a6b4cdba553b966c26cd7bf290e2946715f28e13
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2020-10-20 08:08:01 +0200

    Removed unused factory method

--- src/test/java/integration/commons/RoadFactory.java
+++ src/test/java/integration/commons/RoadFactory.java
@@ -1,12 +1,9 @@
 package integration.commons;
 
 import dev.przbetkier.routemesh.domain.node.Node;
-import dev.przbetkier.routemesh.domain.obstacle.Obstacle;
 import dev.przbetkier.routemesh.domain.road.Road;
 import dev.przbetkier.routemesh.domain.road.RoadBuilder;
 
-import java.util.Collections;
-import java.util.Set;
 import java.util.TreeSet;
 
 import static dev.przbetkier.routemesh.domain.road.RoadDirection.TWO_WAY;
@@ -35,22 +32,4 @@ public class RoadFactory {
                 .withWidth(7000)
                 .build();
     }
-
-    public static Road simpleWithObstacles(String name, Node start, Node end, Set<Obstacle> obstacles) {
-        return RoadBuilder.builder()
-                .withName(name)
-                .withStartNode(start)
-                .withEndNode(end)
-                .withDirection(TWO_WAY)
-                .withAdmins(emptySet())
-                .withType(G)
-                .withNumbers(emptySet())
-                .withKmRange(new TreeSet<>(asList(11.111, 222.22)))
-                .withLines(4)
-                .withMaxAxleLoad(123.0)
-                .withWidth(7000)
-                .withObstacles(obstacles)
-                .build();
-    }
-
 }
